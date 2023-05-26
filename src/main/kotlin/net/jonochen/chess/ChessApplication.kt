048da7c7ee4365e6b3dec0b5cd4a924a2c726d9a
Jonathan Chen <jqc3@cornell.edu>
2023-05-26 12:14:38 -0400

    Initial commit

---
+++ src/main/kotlin/net/jonochen/chess/ChessApplication.kt
@@ -0,0 +1,11 @@
+package net.jonochen.chess
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChessApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChessApplication>(*args)
+}
